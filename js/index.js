df338d92092b580eb1a293a9bf4c586b182d1120
Phong <ttphong1331985@yahoo.com>
2021-06-16 14:29:07 +1000

    write comments task 6,7

--- js/index.js
+++ js/index.js
@@ -1,99 +1,182 @@
+// Initialize a new TaskManager with currentId set to 0
 const taskManager = new TaskManager(0);
+///////////// add first load data
+taskManager.addTask('Take trash', 'take out the trash to the front of the house', 'Nick', '2020-09-20','In Progress');
+taskManager.addTask('Cook Dinner','Prepare a healthy serving of pancakes for the family tonight','Nick','2020-09-20','In Progress');
+taskManager.addTask('Go Shopping','Buy Banana and other items from supermarket','Dave','2020-09-20','Done');
+console.log(taskManager);
+taskManager.printDiv();
+////////////
+///TASK 8: "Mark as done" button is added to the tasks HTML template created in Task 7 and is visible in each task card on the page.
+var numclick = 0;
+function markDoneDiv(id){
+  numclick = 1;
+  for (let i = 0; i < taskManager.tasks.length; i++) {
+    let taskID = taskManager.tasks[i].task.id;
+    if(id === taskID){
+      taskManager.tasks[i].task.status = 'Done';
+      console.log(taskManager.tasks[i].task.status);
+      taskManager.printDiv();
+      break;
+    }
+    else{
+    console.log('No Match');
+    }
+  }
+}
 
 
-taskManager.markDone(2);
-const txtTaskName =  document.querySelector('#txtTaskName');
-const txtTaskDescription =  document.querySelector('#txtTaskDescription');
-const txtDate =  document.querySelector('#txtDate');
-const txtAssignedTo =  document.querySelector('#txtAssignedTo');
-const selectStatus =  document.querySelector('#selectStatus');
-const btnAddTask =  document.querySelector('#btnAddTask');
+// Select the New Task Form
+const form = document.querySelector("#addTaskForm");
 
-// errMessage 
-const errMessageName =  document.querySelector('#errMessageName');
-const errMessageDescription =  document.querySelector('#errMessageDescription');
-const errMessageDate =  document.querySelector('#errMessageDate');
-const errMessageAssigned =  document.querySelector('#errMessageAssigned');
-const errMessageStatus =  document.querySelector('#errMessageStatus');
+// Add an 'onsubmit' event listener
+form.addEventListener("submit", (event) => {
+  
+
+  let txtTaskName =  document.querySelector('#txtTaskName');
+  let txtTaskDescription =  document.querySelector('#txtTaskDescription');
+  let txtDate =  document.querySelector('#txtDate');
+  let txtAssignedTo =  document.querySelector('#txtAssignedTo');
+  let selectStatus =  document.querySelector('#selectStatus');
+  //let btnAddTask =  document.querySelector('#btnAddTask');
+
+  //err message
+  
+  let errMessageName =  document.querySelector('#errMessageName');
+  let errMessageDescription =  document.querySelector('#errMessageDescription');
+  let errMessageDate =  document.querySelector('#errMessageDate');
+  let errMessageAssigned =  document.querySelector('#errMessageAssigned');
+  let errMessageStatus =  document.querySelector('#errMessageStatus');
 
 
+  let validationFail = 0;
 
+  // Prevent default action
+  event.preventDefault();
 
-function validFormFieldInput(){
-    console.log(`Name: ${txtTaskName.value}`);
-    console.log(`Description: ${txtTaskDescription.value}`);
-    console.log(`Date: ${txtDate.value}`);
-    console.log(`Assigned to: ${txtAssignedTo.value}`);
-    console.log(`Status: ${selectStatus.value}`);
-    console.log(`Name: ${txtTaskName.value.length}`);
+  // Call this to clear all the form fields after the submission
+  const clearFormFields = () => {
+    txtTaskName.value = "";
+    txtTaskDescription.value = "";
+    txtAssignedTo.value = "";
+    txtDate.value = "";
+    selectStatus.value = ""
+
+  };
+  // Clear error after 1.6 s
+  const clearError= () =>{
+    setTimeout(function(){
+      errMessageName.innerHTML = "";
+      errMessageDate.innerHTML = "";
+      errMessageDescription.innerHTML = "";
+      errMessageStatus.innerHTML = "";
+      errMessageAssigned.innerHTML = "";
+  }, 1600);
+
+  };
+
+  console.log("Task Name :" + txtTaskName.value.length);
+  console.log("Task Description :" + txtTaskDescription.value.length);
+  console.log("Task Assigned To :" + txtAssignedTo.value.length);
+  console.log("Task Due Date :" + txtDate.value);
+  console.log("Task Status:" + selectStatus.value);
+
+  ////////////// 
 // 1. Check if the Task Name input value is more than 5 characters.
 // 2. Check if the Task Description input value is more than 5 characters.
 // 3. Check if the Assigned To value is more than 5 characters.
 // 4. Check if the Task Due Date input value is not empty.
 // 5. Check if the Task Status input value is not empty.
-    if(txtTaskName.value.length < 5){
-        
-        errMessageName.innerHTML = "Please input more than 5 character";
-        errMessageName.style.color= "red";
-        txtTaskName.style.borderColor = "red";
-    } else {
-        errMessageName.innerHTML = "Well done!";
-        errMessageName.style.color= "green";
-        txtTaskName.style.borderColor = "green";
-    }
-    // condition of Task Description
-    if(txtTaskDescription.value.length < 5){
-        
-        errMessageDescription.innerHTML = "Please input more than 5 character";
-        errMessageDescription.style.color= "red";
-        txtTaskDescription.style.borderColor = "red";
-    } else {
-        errMessageDescription.innerHTML = "Well done!";
-        errMessageDescription.style.color= "green";
-        txtTaskDescription.style.borderColor = "green";
-    }
 
-    if(txtAssignedTo.value.length < 5){
-        
-        errMessageAssigned.innerHTML = "Please input more than 5 character";
-        errMessageAssigned.style.color= "red";
-        txtAssignedTo.style.borderColor = "red";
-    } else {
-        errMessageAssigned.innerHTML = "Well done!";
-        errMessageAssigned.style.color= "green";
-        txtAssignedTo.style.borderColor = "green";
-    }
+  
+  if(txtTaskName.value.length < 5 || txtTaskName.value === ""){
+    errMessageName.innerHTML = "Please input more than 5 characters";
+    errMessageName.style.color= "red";
+    txtTaskName.style.borderColor = "red";
+    validationFail++;
+    
+    
+  } else {
+    
+    errMessageName.innerHTML = "Well done!";
+    errMessageName.style.color= "green";
+    txtTaskName.style.borderColor = "green";
+  }
 
-    if(txtDate.value == "" ){
-        //console.log('i am in date');
-        errMessageDate.innerHTML = "Please provide a valid date";
-       
-        errMessageDate.style.color= "red";
-        txtDate.style.borderColor = "red";
-    } else {
-        errMessageDate.innerHTML = "Well done!";
-       
-        errMessageDate.style.color= "green";
-        txtDate.style.borderColor = "green";
+  // condition of Task Description
+  if(txtTaskDescription.value.length < 5){
+    errMessageDescription.innerHTML = "Please input more than 5 characters";
+    errMessageDescription.style.color= "red";
+    txtTaskDescription.style.borderColor = "red";
+    validationFail++;
+  } else {
+    errMessageDescription.innerHTML = "Well done!";
+    errMessageDescription.style.color= "green";
+    txtTaskDescription.style.borderColor = "green";
+    
+  }
+  
+  if(txtAssignedTo.value.length < 5){
+    errMessageAssigned.innerHTML = "Please input more than 5 characters";
+    errMessageAssigned.style.color= "red";
+    txtAssignedTo.style.borderColor = "red";
+    validationFail++;
+  } else {
+    errMessageAssigned.innerHTML = "Well done!";
+    errMessageAssigned.style.color= "green";
+    txtAssignedTo.style.borderColor = "green";
+    
+  }
+  
+  if(txtDate.value == "" ){
+    //console.log('i am in date');
+    errMessageDate.innerHTML = "Please provide a valid date";
+   
+    errMessageDate.style.color= "red";
+    txtDate.style.borderColor = "red";
+    validationFail++;
+  } else {
+    errMessageDate.innerHTML = "Well done!";
+   
+    errMessageDate.style.color= "green";
+    txtDate.style.borderColor = "green";
+    
+  
+  }
+  if(selectStatus.value === "Choose the Status"){
+    //console.log('i am in status');
+    errMessageStatus.innerHTML = "Please choose status";
+   
+    errMessageStatus.style.color= "red";
+    selectStatus.style.borderColor = "red";
+    validationFail++;
+  } else {
+    errMessageStatus.innerHTML = "Well done!";    
+    errMessageStatus.style.color= "green";
+    selectStatus.style.borderColor = "green";
+            
+  }
 
-    }
-    if(selectStatus.value === "Choose the Status"){
-        //console.log('i am in status');
-        errMessageStatus.innerHTML = "Please choose status";
-       
-        errMessageStatus.style.color= "red";
-        selectStatus.style.borderColor = "red";
-    } else {
-        errMessageStatus.innerHTML = "Well done!";
-       
-        errMessageStatus.style.color= "green";
-        selectStatus.style.borderColor = "green";
-    }
-}
 
+  
+  if (validationFail > 0) {
+    validationFail = 0;
+    clearError();
+    clearFormFields();
+    return;
+  } else {
+    // Push the valid input into our tasks array
+    taskManager.addTask(
+        txtTaskName.value,
+        txtTaskDescription.value,
+        txtAssignedTo.value,
+        txtDate.value,
+        selectStatus.value
+    );
+    clearFormFields();
+    clearError();
+    errMessageAssigned.innerHTML = "";
+    taskManager.printDiv();
+  }
 
-//btnAddTask.addEventListener("click",validFormFieldInput);
-// import TaskManager from './taskManager.js'
-// const toiditimem = new TaskManager();
-// var TaskManager = require("taskManager.js");
-// btnAddTask.addEventListener('click', toiditimem.addTask(txtTaskName.value,txtTaskDescription.value,txtDate.value));
+});
